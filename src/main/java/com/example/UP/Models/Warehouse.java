77f0c03e60578206e1ddfd1931aed1b7469fea56
Vladimir <isip_v.i.malahov@mpt.ru>
2022-11-02 14:04:58 +0300

    Final Initial Commit

---
+++ src/main/java/com/example/UP/Models/Warehouse.java
@@ -0,0 +1,56 @@
+package com.example.UP.Models;
+
+import javax.persistence.*;
+import java.util.Collection;
+
+@Entity
+@Table(name = "Warehouse_")
+public class Warehouse {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String address;
+    private Integer capacity;
+
+    @OneToMany(mappedBy = "warehouse", fetch = FetchType.EAGER)
+    private Collection<Product> product;
+
+    public Warehouse(String address, Integer capacity){
+        this.address = address;
+        this.capacity = capacity;
+    }
+
+    public Warehouse() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Integer getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(Integer capacity) {
+        this.capacity = capacity;
+    }
+
+    public Collection<Product> getProduct() {
+        return product;
+    }
+
+    public void setProduct(Collection<Product> product) {
+        this.product = product;
+    }
+}
